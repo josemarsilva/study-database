fcdc8ae7e9da5a00c5a8c59aa563c18628faaff5
Josemar <josemarsilva-inmetrics@bitbucket.org>
2019-03-26 18:06:42 -0300

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# README study-database
+
+## 1. Introdução ##
+
+Este repositório de estudo sobre **Banco de dados**
+
+
+### 2. Mapa Mental
+
+![Mapa Mental](doc/images/MindMap%20-%20DatabaseStudy.png) 
+
+
+## Referências ##
+
+* n/a
---
+++ doc/images/MindMap - DatabaseStudy.mm
@@ -0,0 +1,37 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1553633692767" ID="ID_913405909" MODIFIED="1553633890635">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <b><font size="5">Database </font></b>
+    </p>
+    <p style="text-align: center">
+      <b><font size="5">Study</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1553633778318" ID="ID_608958870" MODIFIED="1553633925098" POSITION="right" STYLE="bubble" TEXT="Oracle">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1553633781880" ID="ID_1697027759" MODIFIED="1553633925098" POSITION="right" STYLE="bubble" TEXT="SqlServer">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1553633785880" ID="ID_886475048" MODIFIED="1553633925098" POSITION="right" STYLE="bubble" TEXT="PostgreSQL">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1553633790264" ID="ID_465919319" MODIFIED="1553633925098" POSITION="right" STYLE="bubble" TEXT="MySQL">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1553633795612" ID="ID_1368667212" MODIFIED="1553633925098" POSITION="right" STYLE="bubble" TEXT="SQLite">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1553633855150" ID="ID_958783709" MODIFIED="1553633931661" POSITION="left" STYLE="bubble" TEXT="T&#xf3;picos">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</map>

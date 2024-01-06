fcc091ed5c996e96d6698358a36be0c76d32a985
Josemar Silva <josemarsilva@yahoo.com.br>
2024-01-06 08:06:27 -0300

    .

---
+++ sqlserver/scripts/query_dm_unused_index.sql
@@ -0,0 +1,28 @@
+-- ----------------------------------------------------------------------------
+-- filename: query_dm_unused_index.sql
+-- purpose : Finding unused index
+-- revision: 2024-01-05 22:04 - josemarsilva@inmetrics.com.br - 
+-- remarks : 
+--			* https://www.sqlshack.com/how-to-identify-and-monitor-unused-indexes-in-sql-server/
+--			* https://learn.microsoft.com/en-us/sql/relational-databases/system-dynamic-management-views/sys-dm-db-index-usage-stats-transact-sql?view=sql-server-ver16
+-- ----------------------------------------------------------------------------
+
+SELECT
+    objects.name AS Table_name,
+    indexes.name AS Index_name,
+    dm_db_index_usage_stats.user_seeks,
+    dm_db_index_usage_stats.user_scans,
+    dm_db_index_usage_stats.user_updates
+FROM
+    sys.dm_db_index_usage_stats
+    INNER JOIN sys.objects ON dm_db_index_usage_stats.OBJECT_ID = objects.OBJECT_ID
+    INNER JOIN sys.indexes ON indexes.index_id = dm_db_index_usage_stats.index_id AND dm_db_index_usage_stats.OBJECT_ID = indexes.OBJECT_ID
+WHERE
+    AND
+    dm_db_index_usage_stats.user_lookups = 0
+    AND
+    dm_db_index_usage_stats.user_seeks = 0
+    AND
+    dm_db_index_usage_stats.user_scans = 0
+ORDER BY
+    dm_db_index_usage_stats.user_updates DESC

eb1e29390e5dca9c987a00ec720acfa2dc05de11
Josemar Silva <josemarsilva@yahoo.com.br>
2024-02-03 12:57:42 -0300

    ./sqlserver/scripts

---
+++ sqlserver/scripts/sp_helpindex2_show_indexes_includes.sql
@@ -0,0 +1,14 @@
+-- ----------------------------------------------------------------------------
+-- filename: sp_helpindex2_show_indexes_includes.sql
+-- purpose : Stored Procedure index structure with includes
+-- revision: 2024-02-02 11:25 - josemarsilva - 
+-- remarks : 
+-- ----------------------------------------------------------------------------
+
+```sql
+sp_helpindex2 cliente
+```
+
+```output
+index_name	type	index_description	index_keys	included_keys
+```
---
+++ sqlserver/scripts/sp_whoisactive.sql
@@ -0,0 +1,10 @@
+-- ----------------------------------------------------------------------------
+-- filename : sp_howisactive.sql
+-- purpose  : Who is active
+-- revision : 2024-02-02 11:25 - josemarsilva - 
+-- remarks  : 
+--          * https://learn.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-who-transact-sql?view=sql-server-ver16
+--          * https://codingsight.com/using-sp_whoisactive-in-sql-server/
+-- ----------------------------------------------------------------------------
+
+sp_whoisactive

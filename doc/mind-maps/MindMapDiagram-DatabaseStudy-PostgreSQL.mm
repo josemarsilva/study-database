1ad7fcd20da443d85290a0c9143a4e04646b3c07
Josemar Silva <josemarsilva@yahoo.com.br>
2024-02-09 17:14:01 -0300

    ./doc

--- doc/mind-maps/MindMapDiagram-DatabaseStudy-PostgreSQL.mm
+++ doc/mind-maps/MindMapDiagram-DatabaseStudy-PostgreSQL.mm
@@ -1,6 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1553633692767" ID="ID_913405909" MODIFIED="1556314336952">
+<node CREATED="1553633692767" ID="ID_913405909" MODIFIED="1707508740386">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -12,12 +12,20 @@
   </body>
 </html></richcontent>
 <font BOLD="true" NAME="SansSerif" SIZE="16"/>
-<node CREATED="1556296662908" ID="ID_1539429095" MODIFIED="1556314288690" POSITION="right" STYLE="fork" TEXT="Instala&#xe7;&#xe3;o">
+<icon BUILTIN="gohome"/>
+<node CREATED="1556296662908" ID="ID_1539429095" MODIFIED="1707508749717" POSITION="right" STYLE="fork" TEXT="Instala&#xe7;&#xe3;o">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1556296802748" ID="ID_106938856" MODIFIED="1556311928928" STYLE="fork" TEXT="Linux Centos (7 e 6)"/>
-<node CREATED="1556296820828" ID="ID_723013676" MODIFIED="1556311494551" STYLE="fork" TEXT="Linux - Ubuntu 18"/>
-<node CREATED="1556296825581" ID="ID_406029642" MODIFIED="1556311631420" STYLE="fork" TEXT="Pacotes RPM"/>
-<node CREATED="1556296681710" ID="ID_730631783" MODIFIED="1556311679277" STYLE="fork">
+<icon BUILTIN="folder"/>
+<node CREATED="1556296802748" ID="ID_106938856" MODIFIED="1707509259414" STYLE="fork" TEXT="Linux Centos (7 e 6)">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556296820828" ID="ID_723013676" MODIFIED="1707509259414" STYLE="fork" TEXT="Linux - Ubuntu 18">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556296825581" ID="ID_406029642" MODIFIED="1707509259413" STYLE="fork" TEXT="Pacotes RPM">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556296681710" ID="ID_730631783" MODIFIED="1707509259413" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -28,8 +36,9 @@
     </p>
   </body>
 </html></richcontent>
+<icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1556296681710" ID="ID_378056166" MODIFIED="1556311494551" STYLE="fork">
+<node CREATED="1556296681710" ID="ID_378056166" MODIFIED="1707509259413" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -40,15 +49,22 @@
     </p>
   </body>
 </html></richcontent>
+<icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1623935016367" ID="ID_1154277387" MODIFIED="1623935019288" TEXT="Cloud">
-<node CREATED="1623935020322" ID="ID_450011413" MODIFIED="1623935030224" TEXT="AWS - Amazon Web Service"/>
-<node CREATED="1623935030904" ID="ID_1089776711" MODIFIED="1623935036710" TEXT="GCP - Google Cloud Platform"/>
+<node CREATED="1623935016367" ID="ID_1154277387" MODIFIED="1707509259412" TEXT="Cloud">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1623935020322" ID="ID_450011413" MODIFIED="1707509264312" TEXT="AWS - Amazon Web Service">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1623935030904" ID="ID_1089776711" MODIFIED="1707509264311" TEXT="GCP - Google Cloud Platform">
+<icon BUILTIN="button_ok"/>
 </node>
 </node>
-<node CREATED="1556296667278" ID="ID_573994403" MODIFIED="1556314292788" POSITION="right" STYLE="fork" TEXT="Configura&#xe7;&#xe3;o">
+</node>
+<node CREATED="1556296667278" ID="ID_573994403" MODIFIED="1707508749717" POSITION="right" STYLE="fork" TEXT="Configura&#xe7;&#xe3;o">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1556296681710" ID="ID_366409386" MODIFIED="1556311494553" STYLE="fork">
+<icon BUILTIN="folder"/>
+<node CREATED="1556296681710" ID="ID_366409386" MODIFIED="1707509269650" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -59,8 +75,9 @@
     </p>
   </body>
 </html></richcontent>
+<icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1556296681710" ID="ID_1588062680" MODIFIED="1556311494553" STYLE="fork">
+<node CREATED="1556296681710" ID="ID_1588062680" MODIFIED="1707509269649" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -71,8 +88,9 @@
     </p>
   </body>
 </html></richcontent>
+<icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1556296681710" ID="ID_98634680" MODIFIED="1556312033935" STYLE="fork">
+<node CREATED="1556296681710" ID="ID_98634680" MODIFIED="1707509269649" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -83,43 +101,42 @@
     </p>
   </body>
 </html></richcontent>
+<icon BUILTIN="button_ok"/>
 </node>
 </node>
-<node CREATED="1556296673483" ID="ID_1899322616" MODIFIED="1556314296256" POSITION="right" STYLE="fork" TEXT="Conex&#xe3;o">
+<node CREATED="1556296673483" ID="ID_1899322616" MODIFIED="1707508749717" POSITION="right" STYLE="fork" TEXT="Conex&#xe3;o">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1556296825581" ID="ID_660343816" MODIFIED="1556311494552" STYLE="fork" TEXT="Linha de comando"/>
-<node CREATED="1556296825581" ID="ID_1806517561" MODIFIED="1556312341771" STYLE="fork" TEXT="JDBC Driver"/>
-<node CREATED="1556296825581" ID="ID_48595023" MODIFIED="1556312350444" STYLE="fork" TEXT="Python LIbrary">
-<node CREATED="1556312544436" ID="ID_1288094587" MODIFIED="1556312553635" TEXT="Pip Library Install"/>
-<node CREATED="1556312555889" ID="ID_517221237" MODIFIED="1556312560498" TEXT="Query Sample"/>
+<icon BUILTIN="folder"/>
+<node CREATED="1556296825581" ID="ID_660343816" MODIFIED="1707509282342" STYLE="fork" TEXT="Linha de comando">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556296825581" ID="ID_1806517561" MODIFIED="1707509282342" STYLE="fork" TEXT="JDBC Driver">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556296825581" ID="ID_48595023" MODIFIED="1707509282342" STYLE="fork" TEXT="Python LIbrary">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1556312544436" ID="ID_1288094587" MODIFIED="1707509287404" TEXT="Pip Library Install">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556312555889" ID="ID_517221237" MODIFIED="1707509287403" TEXT="Query Sample">
+<icon BUILTIN="button_ok"/>
 </node>
 </node>
-<node CREATED="1556296667278" ID="ID_683362039" MODIFIED="1556314301357" POSITION="right" STYLE="fork" TEXT="Performance">
+</node>
+<node CREATED="1556296667278" ID="ID_683362039" MODIFIED="1707508749716" POSITION="right" STYLE="fork" TEXT="Performance">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1556296681710" ID="ID_1438446666" MODIFIED="1556313508638" STYLE="fork">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      Particionamento
-    </p>
-  </body>
-</html></richcontent>
+<icon BUILTIN="folder"/>
 </node>
-<node CREATED="1556296681710" ID="ID_299452505" MODIFIED="1556313508638" STYLE="fork">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      Ferramenta profile
-    </p>
-  </body>
-</html></richcontent>
+<node CREATED="1556296667278" ID="ID_775988818" MODIFIED="1707508791425" POSITION="right" STYLE="fork" TEXT="Scripts">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="folder"/>
+<node CREATED="1556312555889" ID="ID_1505498212" MODIFIED="1707509459819" TEXT="Migration (deploy.ps1)">
+<icon BUILTIN="button_ok"/>
 </node>
+</node>
+<node CREATED="1556296667278" ID="ID_1399984052" MODIFIED="1707508791424" POSITION="right" STYLE="fork" TEXT="Laborat&#xf3;rios">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="folder"/>
 </node>
 </node>
 </map>
--- doc/mind-maps/MindMapDiagram-DatabaseStudy-SQLServer.mm
+++ doc/mind-maps/MindMapDiagram-DatabaseStudy-SQLServer.mm
@@ -1,6 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1553633692767" ID="ID_913405909" MODIFIED="1702060245210">
+<node CREATED="1553633692767" ID="ID_913405909" MODIFIED="1707509357758">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -10,13 +10,15 @@
       SQL Server
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="gohome"/>
 <node CREATED="1556296662908" ID="ID_1539429095" MODIFIED="1556314288690" POSITION="right" STYLE="fork" TEXT="Instala&#xe7;&#xe3;o">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1623935016367" ID="ID_1154277387" MODIFIED="1702060267089" TEXT="Windows"/>
-<node CREATED="1623935016367" ID="ID_1702781342" MODIFIED="1702060310489" TEXT="Docker / Container"/>
+<node CREATED="1623935016367" ID="ID_1702781342" MODIFIED="1707507885547" TEXT="Docker / Container">
+<icon BUILTIN="button_ok"/>
+</node>
 <node CREATED="1623935016367" ID="ID_401588427" MODIFIED="1623935019288" TEXT="Cloud">
 <node CREATED="1623935020322" ID="ID_1144124277" MODIFIED="1702060136858" TEXT="Microsoft Azure Portal"/>
 </node>
@@ -82,19 +84,6 @@
 </html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1556296681710" ID="ID_976000402" MODIFIED="1702059956039" STYLE="fork">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      Query Tuning
-    </p>
-  </body>
-</html></richcontent>
-<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
-</node>
 <node CREATED="1556296681710" ID="ID_1956244415" MODIFIED="1702060003724" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -107,6 +96,91 @@
   </body>
 </html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1707507919815" ID="ID_991767569" MODIFIED="1707507924192" TEXT="Query Store"/>
+<node CREATED="1707507941024" ID="ID_1610290973" MODIFIED="1707507967413" TEXT="sys.dm views">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1556296667278" ID="ID_775988818" MODIFIED="1707508771284" POSITION="right" STYLE="fork" TEXT="Scripts">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1556296681710" ID="ID_1038749290" MODIFIED="1707508111080" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Active Sessions
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556296681710" ID="ID_314181433" MODIFIED="1707508069293" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Query sys.dm_*
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556296681710" ID="ID_1902611747" MODIFIED="1707508152132" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Indexes
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556296681710" ID="ID_791718253" MODIFIED="1707508138514" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Statistics IO, Time
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1556296667278" ID="ID_1399984052" MODIFIED="1707508014132" POSITION="right" STYLE="fork" TEXT="Laborat&#xf3;rios">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1556296681710" ID="ID_1800739747" MODIFIED="1707508311253" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      How to Pin table
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1707508295943" ID="ID_723338876" MODIFIED="1707508311253" TEXT="How to Table Partition">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1707508295943" ID="ID_368544155" MODIFIED="1707508360796" TEXT="Lab Fake SQLServer statistics">
+<icon BUILTIN="button_ok"/>
 </node>
 </node>
 </node>

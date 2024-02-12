0db2efb79eca9681c210e46033783e343c6d4497
Josemar Silva <josemarsilva@yahoo.com.br>
2024-02-12 19:01:36 -0300

    ./sqlserver/scripts ren migration -> powershell-developer + ./migrate-database-from-source-to-target

---
+++ doc/mind-maps/MindMapDiagram-DatabaseStudy-Oracle.mm
@@ -0,0 +1,141 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1553633692767" ID="ID_913405909" MODIFIED="1707775132523">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Oracle
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="gohome"/>
+<node CREATED="1556296662908" ID="ID_1539429095" MODIFIED="1556314288690" POSITION="right" STYLE="fork" TEXT="Instala&#xe7;&#xe3;o">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1623935016367" ID="ID_1154277387" MODIFIED="1702060267089" TEXT="Windows"/>
+<node CREATED="1623935016367" ID="ID_1702781342" MODIFIED="1707775141526" TEXT="Docker / Container"/>
+<node CREATED="1623935016367" ID="ID_401588427" MODIFIED="1623935019288" TEXT="Cloud">
+<node CREATED="1623935020322" ID="ID_1144124277" MODIFIED="1702060136858" TEXT="Microsoft Azure Portal"/>
+</node>
+<node CREATED="1707775149737" ID="ID_1133545943" MODIFIED="1707775164255" TEXT="VirtualBox">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1556296667278" ID="ID_573994403" MODIFIED="1556314292788" POSITION="right" STYLE="fork" TEXT="Configura&#xe7;&#xe3;o">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1556296681710" ID="ID_366409386" MODIFIED="1556311494553" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Regra de firewall
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1556296681710" ID="ID_1588062680" MODIFIED="1556311494553" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Conectividade
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1556296681710" ID="ID_98634680" MODIFIED="1556312033935" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Cria&#231;&#227;o database e user
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1556296673483" ID="ID_1899322616" MODIFIED="1556314296256" POSITION="right" STYLE="fork" TEXT="Conex&#xe3;o">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1556296825581" ID="ID_660343816" MODIFIED="1707775197012" STYLE="fork" TEXT="Linha de comando">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556296825581" ID="ID_1146789473" MODIFIED="1707775200344" STYLE="fork" TEXT="SQLDeveloper">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1556296825581" ID="ID_48595023" MODIFIED="1556312350444" STYLE="fork" TEXT="Python LIbrary"/>
+<node CREATED="1702060165101" ID="ID_255775299" MODIFIED="1702060169601" TEXT="Node JS"/>
+</node>
+<node CREATED="1556296667278" ID="ID_683362039" MODIFIED="1556314301357" POSITION="right" STYLE="fork" TEXT="Performance">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1556296681710" ID="ID_1236140596" MODIFIED="1702060018150" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <i>Troubleshooting</i>
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1556296681710" ID="ID_1956244415" MODIFIED="1702060003724" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ferramentas
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1556296667278" ID="ID_775988818" MODIFIED="1707508771284" POSITION="right" STYLE="fork" TEXT="Scripts">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1556296681710" ID="ID_791718253" MODIFIED="1707775234944" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      AWR Report
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1556296667278" ID="ID_1399984052" MODIFIED="1707508014132" POSITION="right" STYLE="fork" TEXT="Laborat&#xf3;rios">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1556296681710" ID="ID_1800739747" MODIFIED="1707775267950" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      RMAN
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</map>

4dd7d54b9c4fefb25aa651f3ff9d91be35c6b0ed
Josemar Silva <josemarsilva@yahoo.com.br>
2024-03-01 16:25:05 -0300

    ./sqlserver/query_version_cumulative_update.sql

--- sqlserver/scripts/query_version_cumulative_update.sql
+++ sqlserver/scripts/query_version_cumulative_update.sql
@@ -1,9 +1,9 @@
 -- ----------------------------------------------------------------------------
 -- filename: query_version_cumulative_update.sql
 -- purpose : Query SQL Server database version installed and cumulative updates
--- revision: 2024-02-27 11:30 - josemarsilva - 
+-- revision: 2024-02-27 18:30 - josemarsilva - 
 -- remarks : 
 --			* https://learn.microsoft.com/pt-br/sql/t-sql/functions/version-transact-sql-configuration-functions?view=sql-server-ver16
 -- ----------------------------------------------------------------------------
 
-SELECT @@VERSION
+SELECT @@SERVERNAME AS servername, @@SERVICENAME AS servicename, @@VERSION AS version

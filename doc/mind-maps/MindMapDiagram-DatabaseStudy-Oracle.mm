bb0c8c27764d9d94f02abdbd8d412a5467ac2738
Josemar Furegatti De Abreu Silva <josemarsilva@inmetrics.com.br>
2025-03-31 10:25:52 -0300

    ./doc/*

--- doc/mind-maps/MindMapDiagram-DatabaseStudy-Oracle.mm
+++ doc/mind-maps/MindMapDiagram-DatabaseStudy-Oracle.mm
@@ -15,20 +15,35 @@
 <icon BUILTIN="gohome"/>
 <node CREATED="1556296662908" ID="ID_1539429095" MODIFIED="1556314288690" POSITION="right" STYLE="fork" TEXT="Instala&#xe7;&#xe3;o">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1623935016367" ID="ID_1154277387" MODIFIED="1702060267089" TEXT="Windows"/>
-<node CREATED="1623935016367" ID="ID_1702781342" MODIFIED="1707775141526" TEXT="Docker / Container"/>
-<node CREATED="1623935016367" ID="ID_401588427" MODIFIED="1623935019288" TEXT="Cloud">
-<node CREATED="1743423928270" ID="ID_1837993886" MODIFIED="1743424097551" TEXT="OCI - Oracle Cloud Infrastructure">
+<node CREATED="1707507740704" ID="ID_430535623" MODIFIED="1743427306640" TEXT="Linux - Oracle">
 <icon BUILTIN="button_ok"/>
 </node>
-</node>
-<node CREATED="1707775149737" ID="ID_1133545943" MODIFIED="1707775164255" TEXT="VirtualBox">
+<node CREATED="1556296681710" ID="ID_730631783" MODIFIED="1743427303125" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Docker
+    </p>
+  </body>
+</html></richcontent>
 <icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1743427203161" ID="ID_251807863" MODIFIED="1743427265074" TEXT="VirtualBox">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1623934917861" ID="ID_1876046023" MODIFIED="1743427267740" TEXT="Cloud">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1743423671037" ID="ID_568823323" MODIFIED="1743427250189" TEXT="OCI - Oracle Cloud Infrastructure">
+<icon BUILTIN="button_ok"/>
+</node>
 </node>
 </node>
 <node CREATED="1556296667278" ID="ID_573994403" MODIFIED="1556314292788" POSITION="right" STYLE="fork" TEXT="Configura&#xe7;&#xe3;o">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1556296681710" ID="ID_366409386" MODIFIED="1556311494553" STYLE="fork">
+<node CREATED="1556296681710" ID="ID_366409386" MODIFIED="1743427324263" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -39,8 +54,9 @@
     </p>
   </body>
 </html></richcontent>
+<icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1556296681710" ID="ID_1588062680" MODIFIED="1556311494553" STYLE="fork">
+<node CREATED="1556296681710" ID="ID_1588062680" MODIFIED="1743427327031" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -51,8 +67,9 @@
     </p>
   </body>
 </html></richcontent>
+<icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1556296681710" ID="ID_98634680" MODIFIED="1556312033935" STYLE="fork">
+<node CREATED="1556296681710" ID="ID_98634680" MODIFIED="1743427329603" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -63,6 +80,7 @@
     </p>
   </body>
 </html></richcontent>
+<icon BUILTIN="button_ok"/>
 </node>
 </node>
 <node CREATED="1556296673483" ID="ID_1899322616" MODIFIED="1556314296256" POSITION="right" STYLE="fork" TEXT="Conex&#xe3;o">
@@ -73,12 +91,12 @@
 <node CREATED="1556296825581" ID="ID_1146789473" MODIFIED="1707775200344" STYLE="fork" TEXT="SQLDeveloper">
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1556296825581" ID="ID_48595023" MODIFIED="1556312350444" STYLE="fork" TEXT="Python LIbrary"/>
+<node CREATED="1556296825581" ID="ID_48595023" MODIFIED="1743427338292" STYLE="fork" TEXT="Python Library"/>
 <node CREATED="1702060165101" ID="ID_255775299" MODIFIED="1702060169601" TEXT="Node JS"/>
 </node>
 <node CREATED="1556296667278" ID="ID_683362039" MODIFIED="1556314301357" POSITION="right" STYLE="fork" TEXT="Performance">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1556296681710" ID="ID_1236140596" MODIFIED="1743424075828" STYLE="fork">
+<node CREATED="1556296681710" ID="ID_1236140596" MODIFIED="1743427357410" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -88,8 +106,7 @@
       README.md
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="folder"/>
 </node>
@@ -99,24 +116,22 @@
 <node CREATED="1743424002005" ID="ID_40491911" MODIFIED="1743424007884" TEXT="Oracle-db-19c-installation-on-oracle-linux-7">
 <icon BUILTIN="folder"/>
 </node>
-<node CREATED="1743424014701" ID="ID_558484842" MODIFIED="1743424018250" TEXT="Oracle-RMAN">
-<icon BUILTIN="folder"/>
-</node>
 </node>
 <node CREATED="1556296667278" ID="ID_1399984052" MODIFIED="1707508014132" POSITION="right" STYLE="fork" TEXT="Laborat&#xf3;rios">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1556296681710" ID="ID_1800739747" MODIFIED="1707775267950" STYLE="fork">
+<node CREATED="1556296681710" ID="ID_1800739747" MODIFIED="1743427422354" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      RMAN
+      <font color="rgb(31, 35, 40)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Noto Sans, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji" size="14px">Oracle-RMAN</font>
     </p>
   </body>
-</html></richcontent>
-<icon BUILTIN="button_ok"/>
+</html>
+</richcontent>
+<icon BUILTIN="folder"/>
 </node>
 </node>
 </node>

24031c54045f4eb5545277a07f68b8d562dca080
Josemar Furegatti De Abreu Silva <josemarsilva@inmetrics.com.br>
2025-06-06 11:27:16 -0300

    oracle

--- oracle/scripts/query_tab_col_ind_statistics.sql
+++ oracle/scripts/query_tab_col_ind_statistics.sql
@@ -10,66 +10,201 @@
 -- Option n. 1: Table Statistics
 --
 
-SELECT table_name, TO_CHAR(last_analyzed, 'DD-MON-YYYY HH24:MI:SSSS') AS last_analyzed, num_rows, blocks, avg_row_len
-FROM   all_tab_statistics
-WHERE  owner = 'STUDY' 
-AND    table_name = 'CUSTOMERS'
-ORDER  BY owner, table_name;
+UNDEFINE table_owner
+UNDEFINE table_name
+DEFINE   table_owner = 'STUDY'
+DEFINE   table_name = 'CUSTOMERS'
+
+SELECT
+	owner,
+    table_name,
+	partition_name,
+	subpartition_name,
+	num_rows, 
+	blocks, 
+	avg_row_len,
+	TO_CHAR(last_analyzed, 'DD-MON-YYYY HH24:MI:SSSS') AS last_analyzed
+FROM
+    all_tab_statistics
+WHERE
+    1=1
+    AND    owner = '&table_owner' 
+    AND    table_name = '&table_name'
+ORDER BY
+    owner, table_name;
 
 
 --
--- Option n. 2: Columns Statistics
+-- Option n. 2: Table Columns Statistics
 --
 
+UNDEFINE table_owner
+UNDEFINE table_name
+UNDEFINE column_name
+DEFINE   table_owner = 'STUDY'
+DEFINE   table_name = 'CUSTOMERS'
+DEFINE   column_name = '%'
        
-SELECT table_name, column_name, num_distinct, TRUNC(density, 6) AS DENSITY, num_nulls, RPAD(UTL_RAW.CAST_TO_VARCHAR2(low_value),10,' ') AS low_value, RPAD(UTL_RAW.CAST_TO_VARCHAR2(high_value),10,' ') AS high_value, RPAD(histogram,10,' ') AS HISTOGRAM, TO_CHAR(last_analyzed, 'DD-MON-YYYY HH24:MI:SSSS') AS last_analyzed
-FROM   all_tab_col_statistics
-WHERE  owner = 'STUDY' 
-AND    table_name IN ('CUSTOMERS')
-ORDER  BY owner, table_name;
+SELECT
+    table_name, 
+	column_name, 
+	num_distinct, 
+	TRUNC(density, 6) AS DENSITY, 
+	num_nulls, 
+	RPAD(UTL_RAW.CAST_TO_VARCHAR2(low_value),10,' ') AS low_value, 
+	RPAD(UTL_RAW.CAST_TO_VARCHAR2(high_value),10,' ') AS high_value, 
+	RPAD(histogram,10,' ') AS HISTOGRAM, 
+	TO_CHAR(last_analyzed, 'DD-MON-YYYY HH24:MI:SSSS') AS last_analyzed
+FROM
+    all_tab_col_statistics
+WHERE  1=1
+    AND    owner = '&table_owner' 
+    AND    table_name = '&table_name'
+    AND    column_name LIKE '&column_name'
+ORDER BY
+    owner, table_name;
 
 
 --
 -- Option n. 3: Index Statistics
 --
 
-SELECT i.table_name,
-       i.index_name, 
-       i.index_type, -- NORMAL, BITMAP, FUNCTION-BASED NORMAL
-       i.uniqueness, -- UNIQUE or NONUNIQUE
-       i.clustering_factor,
-       i.num_rows,
-       i.leaf_blocks,
-       i.distinct_keys,
-       i.avg_leaf_blocks_per_key,
-       i.avg_data_blocks_per_key,
-       TO_CHAR(i.last_analyzed, 'DD-MON-YYYY HH24:MI:SS') AS last_analyzed
-FROM   all_indexes i
-WHERE  i.owner = 'STUDY' 
-  AND  i.table_name = 'CUSTOMERS'
+UNDEFINE table_owner
+UNDEFINE table_name
+UNDEFINE index_name
+DEFINE   table_owner = 'STUDY'
+DEFINE   table_name = 'CUSTOMERS'
+DEFINE   index_name = '%'
+
+SELECT
+	i.table_name,
+	i.index_name, 
+	i.index_type, -- NORMAL, BITMAP, FUNCTION-BASED NORMAL
+	i.uniqueness, -- UNIQUE or NONUNIQUE
+	i.clustering_factor,
+	i.num_rows,
+	i.leaf_blocks,
+	i.distinct_keys,
+	i.avg_leaf_blocks_per_key,
+	i.avg_data_blocks_per_key,
+	TO_CHAR(i.last_analyzed, 'DD-MON-YYYY HH24:MI:SS') AS last_analyzed
+FROM
+	all_indexes i
+WHERE
+	i.owner = '&table_owner' 
+	AND  i.table_name = '&table_name'
+	AND  i.index_name LIKE '&index_name'
 ORDER  BY i.table_name, i.index_name;
 
 
 --
--- Option n. 4: Indexed Columns Statistics
+-- Option n. 4: Table Partitions
 --
 
-SELECT i.index_name,
-       ic.column_name,
-       ic.column_position,
-       i.distinct_keys,
-       i.num_rows,
-       ic.column_length,
-       i.uniqueness, -- UNIQUE or NONUNIQUE
-       i.index_type  -- NORMAL, BITMAP, FUNCTION-BASED NORMAL
-FROM   all_indexes i
-JOIN   all_ind_columns ic ON i.index_name = ic.index_name AND i.owner = ic.index_owner
-WHERE  i.owner = 'STUDY'
-  AND  i.table_name = 'CUSTOMERS'
-ORDER  BY i.index_name, ic.column_position;
+UNDEFINE table_owner
+UNDEFINE table_name
+UNDEFINE partition_name
+DEFINE   table_owner = 'STUDY'
+DEFINE   table_name = 'P_RANGE_ORDERS'
+DEFINE   partition_name = '%'
+
+SELECT
+	table_owner,
+    table_name,
+	partition_position,
+	partition_name,
+	subpartition_count,
+	composite,
+	high_value,
+	num_rows,
+	TO_CHAR(last_analyzed, 'DD-MON-YYYY HH24:MI:SS') AS last_analyzed
+FROM
+    all_tab_partitions
+WHERE
+	table_owner = '&table_owner' 
+    AND table_name = '&table_name'
+	AND partition_name LIKE '&partition_name'
+ORDER BY
+    table_owner, table_name, partition_position;
+
+
 
 --
--- Option n. 5: TAB_STATISTICS
+-- Option n. 5: Index Partitions
+--
+
+
+UNDEFINE table_owner
+UNDEFINE table_name
+UNDEFINE index_name
+DEFINE   table_owner = 'STUDY'
+DEFINE   table_name = 'P_RANGE_ORDERS'
+DEFINE   index_name = '%'
+
+SELECT
+	i.index_name,
+	i.index_type,
+	i.table_owner,
+	i.table_name,
+	i.partitioned,
+	ip.partition_position,
+	ip.partition_name,
+	ip.subpartition_count,
+	ip.high_value,
+	ip.status,
+	ip.num_rows,
+	TO_CHAR(ip.last_analyzed, 'DD-MON-YYYY HH24:MI:SS') AS last_analyzed
+FROM
+    all_indexes i,
+	all_ind_partitions ip
+WHERE
+	i.table_owner = '&table_owner' 
+    AND i.table_name = '&table_name'
+	AND i.index_name LIKE '&index_name'
+ORDER BY
+	i.table_owner,
+	i.table_name,
+    i.index_name	
+;
+
+
+--
+-- Option n. 6: Indexed Columns and expressions Statistics
+--
+
+UNDEFINE table_owner
+UNDEFINE table_name
+UNDEFINE index_name
+DEFINE   table_owner = 'STUDY'
+DEFINE   table_name = 'CUSTOMERS'
+DEFINE   index_name = '%'
+
+SELECT
+	i.table_owner,
+	i.table_name,
+	i.index_name,
+	ic.column_position,
+    ic.column_name,
+	i.distinct_keys,
+	i.num_rows,
+	ic.column_length,
+	i.uniqueness, -- UNIQUE or NONUNIQUE
+	i.index_type  -- NORMAL, BITMAP, FUNCTION-BASED NORMAL
+FROM all_indexes i
+JOIN all_ind_columns ic ON i.index_name = ic.index_name AND i.owner = ic.index_owner
+LEFT OUTER JOIN all_ind_expressions ie ON ie.index_name = ic.index_name AND ie.column_position = ic.column_position
+WHERE
+	i.owner = '&table_owner' 
+	AND  i.table_name = '&table_name'
+	AND  i.index_name LIKE '&index_name'
+ORDER BY
+	i.table_owner,
+	i.table_name,
+	i.index_name
+;
+
+--
+-- Option n. 7: TAB_STATISTICS
 --
 
 SELECT owner, table_name, partition_name, partition_position, subpartition_name, object_type, num_rows, blocks, empty_blocks, avg_row_len, sample_size, last_analyzed
@@ -78,7 +213,7 @@ WHERE  owner = 'STUDY' AND table_name = 'CUSTOMERS';
 
 
 --
--- Option n. 6: TAB_HISTOGRAMS
+-- Option n. 8: TAB_HISTOGRAMS
 --
 
 SELECT column_name, endpoint_number, endpoint_value, endpoint_actual_value
